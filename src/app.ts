635fc8c313055d1bf54bff3226570483e2cec8d9
Hiranmay Mushahary <hiranmaymushahary@MacBookAir.lan>
2026-07-01 20:18:03 +0530

    Added basic prisma setup

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,15 @@
 import express, {Express} from "express";
 
-export const app: Express = express();
+const app: Express = express();
 
 app.get("/health", (_req, res) => {
-    res.json({ status: "OK",
+    res.json({ status: "OK!!!##",
         timestamp: new Date().toISOString(),
      });
 });
+
+export { app };
+
+
+
+
