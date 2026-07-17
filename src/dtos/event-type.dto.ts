a31232b08e2da4d30a51447eab1f9eca1a5163e4
Hiranmay Mushahary <hiranmaymushahary@MacBookAir.lan>
2026-07-17 11:34:59 +0530

    Added event type repository CRUD operations

---
+++ src/dtos/event-type.dto.ts
@@ -0,0 +1,25 @@
+import {z} from "zod";
+
+
+export const createEventTypeSchema = z.object({
+    title  :            z.string().min(1).max(200),
+    description  :      z.string().max(1000).optional(),
+    durationMinutes  :  z.number().min(15).max(120).default(30),
+    isActive    :       z.boolean().default(true),
+    locationType :      z.enum(["online" , "inperson"]).default("online"),  
+    locationValue  :    z.string().optional(),
+    bufferBeforeMinutes : z.number().min(0).max(120).default(0),
+    bufferAfterMinutes  : z.number().min(0).max(120).default(0), 
+
+});
+
+export const UpdateEventTypeSchema = createEventTypeSchema.partial();
+
+export type CreateEventTypeDto = z.infer<typeof createEventTypeSchema>;
+
+
+export type UpdateEventTypeDto = z.infer<typeof UpdateEventTypeSchema>;
+
+
+
+
